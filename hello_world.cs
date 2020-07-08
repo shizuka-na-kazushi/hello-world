8e29e1c7e1af9879e55835405a70bac5689ca091
kazushi <Kazushi.Yoshida@outlook.com>
2020-07-08 21:41:27 +0900

    first version of hello world!s

---
+++ hello_world.cs
@@ -0,0 +1,12 @@
+using System;
+namespace hello_world
+{
+  class hello_world
+  {
+    static void Main()
+    {
+      Console.WriteLine("hello, world!");
+    }
+  }
+}
+
---
+++ hello_world.vb
@@ -0,0 +1,8 @@
+Imports System
+
+Module hello_world
+  Sub Main(args As String())
+    Console.WriteLine("hello, world!")
+  End Sub
+End Module
+
